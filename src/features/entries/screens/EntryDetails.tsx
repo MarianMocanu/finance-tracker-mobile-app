75db35263af246fc740e79cc9fde0b193a9473c2
Fryderyk Boncler <f.relcnob@gmail.com>
2024-03-03 20:55:12 +0100

    feat: added fetch details by id from route

--- src/features/entries/screens/EntryDetails.tsx
+++ src/features/entries/screens/EntryDetails.tsx
@@ -1,24 +1,59 @@
-import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { colors } from '@globals/style';
+import { useEntry } from '@queries/Entries';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
+import { FC, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
-const EntryDetails: FC = () => {
-  return (
-    <View style={style.container}>
-      <Text>Here is the entry details screen</Text>
-    </View>
-  );
+type Props = {
+  id: number;
+};
+
+const EntryDetails: FC = (dispatch: any) => {
+  const { data: entry, isLoading } = useEntry(dispatch.route.params.id);
+
+  if (isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color={colors.blue.dark} />
+      </View>
+    );
+  }
+
+  if (!isLoading && !entry) {
+    return (
+      <View style={styles.container}>
+        <Text>No data for this entry found</Text>
+      </View>
+    );
+  }
+
+  if (entry) {
+    console.log(entry);
+    return (
+      <View style={styles.container}>
+        <Text>{entry.amount}</Text>
+      </View>
+    );
+  }
 };
 
 export default EntryDetails;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+
   text: {
     fontSize: 18,
     lineHeight: 22,
+  },
+
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- src/queries/Entries.ts
+++ src/queries/Entries.ts
@@ -4,6 +4,7 @@ import { Entry } from '@models';
 
 export const QUERY_KEYS = {
   ALL_ENTRIES: 'all-entries',
+  SINGLE_ENTRY: 'single-entry',
 };
 
 export const useEntries = (): UseQueryResult<Entry[], Error> => {
@@ -12,6 +13,16 @@ export const useEntries = (): UseQueryResult<Entry[], Error> => {
     queryFn: async function fetchAllEntries() {
       const data = (await axios.get('/entry')).data;
       return data as Entry[];
+    },
+  });
+};
+
+export const useEntry = (id: number): UseQueryResult<Entry, Error> => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SINGLE_ENTRY],
+    queryFn: async function fetchSingleEntry() {
+      const data = (await axios.get(`/entry/${id}`)).data;
+      return data as Entry;
     },
   });
 };
