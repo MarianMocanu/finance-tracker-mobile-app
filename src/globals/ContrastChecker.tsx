e315319d17d1c59f669b8f6f91d968842c27efc3
Fryderyk Boncler <f.relcnob@gmail.com>
2024-03-08 07:38:39 +0100

    feat: added contrast checker function

---
+++ src/globals/ContrastChecker.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+type Props = {
+  hexColor: string;
+};
+
+const ContrastChecker = (hexColor: string) => {
+  const r = parseInt(hexColor.slice(1, 3), 16);
+  const g = parseInt(hexColor.slice(3, 5), 16);
+  const b = parseInt(hexColor.slice(5, 7), 16);
+
+  const lightness = r + g + b;
+
+  // returns true if value is light (fx. white)
+  return lightness > 128 ? true : false;
+};
+
+export default ContrastChecker;
